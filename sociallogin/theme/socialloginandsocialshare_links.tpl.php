e7c6e09c8048a4a6e70b1859ad9a96715a2e5ddc
sidhujag <sidhujag@hotmail.com>
2015-02-03 14:25:50 -0800

    Revert "Bitshares Login"
    This reverts commit de09a505330248e88af7c995e4904742a97a55fc.

--- sociallogin/theme/socialloginandsocialshare_links.tpl.php
+++ sociallogin/theme/socialloginandsocialshare_links.tpl.php
@@ -1,6 +1,4 @@
-
-
-<?php	  
+<?php
 /**
  * Theme social links.
  **/
@@ -21,18 +19,5 @@ $my_settings = array(
   </script>
 <?php
 }
-if (variable_get('socialloginandsocialshare_bitshareslogin_enabled') == 1) {
-$my_settings = array(
-  'location' => $loc,
-  'bitshareslogin_handler' => url('socialloginandsocialshare/bitshareslogin_handler', array('absolute' => TRUE))
-);
 ?>
-<script type="text/javascript"
-      src="<?php print $GLOBALS['base_url'] ?>/<?php echo drupal_get_path('module', 'socialloginandsocialshare') ?>/js/sociallogin_bitsharesloginbutton.js">
-</script>
-<div class="cell text-center">
-      <a href="javascript:void(0)" onclick="javascript:getBitsharesLoginURL('<?php echo $my_settings['location']; ?>', '<?php echo $my_settings['bitshareslogin_handler']; ?>')" class="btn btn-block btn-lg btn-social btn-bitshares">
-        <img alt="BTS" height="42" src="<?php echo $GLOBALS['base_url']; ?>/<?php echo drupal_get_path('module', 'socialloginandsocialshare'); ?>/images/logo-bitshares.svg" width="42">&nbsp;BitShares Login</a>
-    </div>
-<?php } ?>    
 <div class="interfacecontainerdiv"></div>
